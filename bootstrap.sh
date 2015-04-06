7cd86de9b0b79648c2c2621aae395502c117afb9
Matt Moretti <werebus@gmail.com>
2015-04-06 10:38:56 -0400

    Failed if gemrc was already toast

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 pacman -S --needed git ruby base-devel --noconfirm
-rm /etc/gemrc
+[ -e /etc/gemrc ] && rm /etc/gemrc
 gem install chef --no-ri --no-rdoc
 
 cd /tmp/chef-bib
